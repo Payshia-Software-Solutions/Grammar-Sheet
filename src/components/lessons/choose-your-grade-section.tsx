01e367c092b47b3accf46f054dcb8a00e7780490
payshia lms <payshialms@gmail.com>
2025-07-17 17:30:13 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent class

--- src/components/lessons/choose-your-grade-section.tsx
+++ src/components/lessons/choose-your-grade-section.tsx
@@ -93,7 +93,7 @@ export function ChooseYourGradeSection() {
             Choose Your Grade
           </h2>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-none mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8 max-w-5xl mx-auto">
           {gradeData.map((item) => {
             const colors = colorClasses[item.color as keyof typeof colorClasses];
             return (
